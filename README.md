34ac367d89b2e6cc386031b1860af0cd9b3bce01
YoloJJ333 <jaydenjjlee812@gmail.com>
2025-11-16 21:26:17 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mod for my survival server
+
+some features copied from my other mc mods
+
+## Features
---
+++ src/main/java/yolojj333/survivalServerMod/SurvivalServerMod.java
@@ -0,0 +1,10 @@
+package yolojj333.survivalServerMod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SurvivalServerMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
