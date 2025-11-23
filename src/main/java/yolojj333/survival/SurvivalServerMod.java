b61d755201bd5c0d998063e88fc44a0a5faa27c3
YoloJJ333 <jaydenjjlee812@gmail.com>
2025-11-22 17:10:41 -0800

    fixing remote

---
+++ src/main/java/yolojj333/survival/SurvivalServerMod.java
@@ -0,0 +1,16 @@
+package yolojj333.survival;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SurvivalServerMod implements ModInitializer {
+	public static final String modid = "survival-server-mod";
+	public static final Logger log = LoggerFactory.getLogger(modid);
+
+	@Override
+	public void onInitialize() {
+		log.info("Hello Fabric world!");
+	}
+}
